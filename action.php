0faa62f2dc6eab40f7276ef0a8b850dddf49f83c
Arshita-Rehal <arshrehal247@gmail.com>
2024-07-03 09:26:52 +0530

    Add files via upload

---
+++ action.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>hello world</h1>
+<?php
+      $x=$_POST["fname"];
+      $y=$_POST["contact"];
+      $z=$_POST["email"];
+      echo $x;
+      echo"<br>";
+      echo $y;
+      echo"<br>";
+      echo $z;
+      
+    ?>  
+</body>
+</html>
+
